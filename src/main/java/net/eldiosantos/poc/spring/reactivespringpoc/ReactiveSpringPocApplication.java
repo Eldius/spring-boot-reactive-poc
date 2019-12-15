e6979eceeb9f0e5095715782dcc8b70f85520f6e
Eldio Santos Junior <eldiosantos@gmail.com>
2019-12-15 21:02:12 +0000

    Added the base Maven Java project...

---
+++ src/main/java/net/eldiosantos/poc/spring/reactivespringpoc/ReactiveSpringPocApplication.java
@@ -0,0 +1,13 @@
+package net.eldiosantos.poc.spring.reactivespringpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/eldiosantos/poc/spring/reactivespringpoc/ReactiveSpringPocApplicationTests.java
@@ -0,0 +1,13 @@
+package net.eldiosantos.poc.spring.reactivespringpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSpringPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
